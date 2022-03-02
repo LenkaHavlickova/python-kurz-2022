97b72d13130b4e78622c1c4d355da69ef7024a8e
LenkaHavlickova <havlickova.lenka.hl@gmail.com>
2022-03-02 13:47:23 +0100

    Ukol01_baliky s rozsirenim

--- ukol01_baliky.py
+++ ukol01_baliky.py
@@ -6,11 +6,42 @@ baliky = {
     "B947X": False,
 }
 
-package = input("Zadej kód balíku: ")
-if package in baliky:
-  if baliky[package] :           #if baliky[package] == True
-    print(f"Balík s kódem {package} byl předán kurýrovi.")
+# package = input("Zadej kód balíku: ")
+# if package in baliky:
+#   if baliky[package] :           #if baliky[package] == True
+#     print(f"Balík s kódem {package} byl předán kurýrovi.")
+#   else:
+#     print(f"Balík s kódem {package} nebyl dosud předán kurýrovi.")
+# else:
+#   print(f"Balík s kódem {package} není evidován v našem systému.")
+
+# Navrh na doplneni
+
+num_package = input ("Zadej kód balíku pro zadaní změny: ")
+if num_package in baliky:
+  if baliky[num_package]:
+    confirm_delivery = input("Balík byl již dříve předán kurýrovi. Byl již balík doručen a záznam o balíku je možné smazat? (ano/ne): ")
+    if confirm_delivery == "ano":
+        baliky.pop(num_package)
+        print(f"Balík s číslem {num_package} byl smazán.")
+        print(baliky)
+    else:
+        print(f"Žádná změna na balíku číslo {num_package} nebyla zaznamenána.")
   else:
-    print(f"Balík s kódem {package} nebyl dosud předán kurýrovi.")
+    confirm_handover = input("Balík ještě nebyl předán kurýrovi. Má být uloženo jeho předání? (ano/ne): ") 
+    if confirm_handover == "ano":
+       baliky[num_package] = True
+       print(f"Balík s číslem {num_package} byl předán.")
+       print(baliky)
+    else:
+        print(f"Žádná změna na balíku číslo {num_package} nebyla zaznamenána.")
 else:
-  print(f"Balík s kódem {package} není evidován v našem systému.")
+  confirm_new = input("Balík s tímto kód zatím není v systému. Chceš balík s tímto kódem založit? (ano/ne): ")
+  if confirm_new == "ano":
+    baliky[num_package] = False
+    print(f"Balík s číslem {num_package} byl uložen do systému")
+    print(baliky)
+  else:
+    print(f"Balík s číslem {num_package} nebyl uložen do systému")
+
+
